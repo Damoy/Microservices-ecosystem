180db91b857734acfb3762ec1c33fc1461dd12b6
FORNALI Damien <itsdamoy@gmail.com>
2018-10-15 23:15:26 +0200

    Clients domain microservices basis

---
+++ clients/clients-addition/clients-accounts-addition/src/main/java/com/lama/mse/client/addition/accounts/app/Application.java
@@ -0,0 +1,9 @@
+package com.lama.mse.client.addition.accounts.app;
+
+public class Application {
+
+	public static void main(String[] args){
+		
+	}
+	
+}
---
+++ clients/clients-addition/clients-orders-addition/src/main/java/com/lama/mse/client/addition/orders/app/Application.java
@@ -0,0 +1,10 @@
+package com.lama.mse.client.addition.orders.app;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
