db10c9f67dcf587280c66c5cb6af995003d8dbde
toutain <xavier96@rocketmail.com>
2018-10-20 16:21:19 +0200

    Restaurant package

---
+++ restaurants/restaurants-modification/restaurants-modification-restaurant/src/main/java/com/lama/mse/restaurants/modification/restaurant/app/Application.java
@@ -0,0 +1,8 @@
+package com.lama.mse.restaurants.modification.restaurant.app;
+
+public class Application {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(Application.class);
+	}
+}
