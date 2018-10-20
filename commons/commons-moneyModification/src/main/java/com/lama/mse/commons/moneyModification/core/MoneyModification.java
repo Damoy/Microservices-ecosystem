c55382036f936e7c9400571c4562f5ea9bdf7d3a
Amandine Benza <amandine.benza@etu.unice.fr>
2018-10-20 15:34:09 +0200

    Simple temporary additions to money-modification

---
+++ commons/commons-moneyModification/src/main/java/com/lama/mse/commons/moneyModification/core/MoneyModification.java
@@ -0,0 +1,14 @@
+package com.lama.mse.commons.moneyModification.core;
+
+public class MoneyModification implements IMoneyModification{
+	
+	public void moneyWitdrawal(String BankAccount, float amount) {
+		System.out.println(amount + "$ were took from the account " + BankAccount);
+	}
+	
+	public void moneyAddition(String RestaurantName, float amount) {
+		System.out.println(amount + "$ were credited to the restaurant " + RestaurantName);
+	}
+
+
+}
