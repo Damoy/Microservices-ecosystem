4d19e341035cad80c99196099abf705e9e235faf
Amandine Benza <amandine.benza@etu.unice.fr>
2018-10-20 13:52:04 +0200

    Update commons modules

--- commons/commons-moneyModification/src/main/java/com/lama/mse/commons/moneyModification/app/Application.java
+++ commons/commons-moneyModification/src/main/java/com/lama/mse/commons/moneyModification/app/Application.java
@@ -1,7 +1,7 @@
-package com.lama.mse.commons.moneyWithdrawal.app;
+package com.lama.mse.commons.moneyModification.app;
 
 public class Application {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
