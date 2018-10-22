e178b4b1db2803e5e9de90341ca5ed90c609dfda
Amandine Benza <amandine.benza@etu.unice.fr>
2018-10-22 16:31:58 +0200

    Modifications pom.xml, add dependencies mongoDB in mse pom

--- clients/clients-accounts-consultation/src/main/java/com/lama/mse/consultation/accounts/repository/IClientRepository.java
+++ clients/clients-accounts-consultation/src/main/java/com/lama/mse/consultation/accounts/repository/IClientRepository.java
@@ -2,10 +2,11 @@ package com.lama.mse.consultation.accounts.repository;
 
 import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 import com.lama.mse.consultation.accounts.model.Client;
 
-public interface IClientRepository // extends MongoRepository<Client, String> 
-{
+public interface IClientRepository extends MongoRepository<Client, String>  {
 	
     public List<Client> findByMail(String mail);
 
--- clients/clients-accounts-modification/src/main/java/com/lama/mse/clients/modification/accounts/repository/IClientRepository.java
+++ clients/clients-accounts-modification/src/main/java/com/lama/mse/clients/modification/accounts/repository/IClientRepository.java
@@ -2,12 +2,13 @@ package com.lama.mse.clients.modification.accounts.repository;
 
 import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 import com.lama.mse.clients.modification.accounts.model.Client;
 
-public interface IClientRepository // extends MongoRepository<Client, String> 
-{
+public interface IClientRepository extends MongoRepository<Client, String> {
 	
     public List<Client> findByMail(String mail);
 
-
+    
 }
