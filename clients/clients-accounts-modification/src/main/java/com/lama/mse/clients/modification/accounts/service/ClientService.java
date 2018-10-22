f038260baf082ee9817b9994d18c8b584d2595ca
FORNALI Damien <itsdamoy@gmail.com>
2018-10-22 17:02:14 +0200

    clients accounts modifs

--- clients/clients-accounts-modification/src/main/java/com/lama/mse/clients/modification/accounts/service/ClientService.java
+++ clients/clients-accounts-modification/src/main/java/com/lama/mse/clients/modification/accounts/service/ClientService.java
@@ -1,35 +1,62 @@
 package com.lama.mse.clients.modification.accounts.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.lama.mse.clients.modification.accounts.model.Client;
+import com.lama.mse.clients.modification.accounts.repository.IClientRepository;
+
+@Service("ClientService")
 public class ClientService implements IClientService {
+	
+	@Autowired
+	private IClientRepository clientRepository;
+	
+	@Autowired
+	private KafkaTemplate<String, Client> kafkaTemplate;
 
-	@Override
-	public void addClient(String email) {
-		// TODO Auto-generated method stub
+	public ClientService() {
 		
+	}
+	
+	public void sendCreateClientMessage(Client client) {
+		kafkaTemplate.send("client-create", client);
 	}
 
 	@Override
-	public void deleteClient(String email) {
-		// TODO Auto-generated method stub
-		
+	public void addClient(Client client) {
+		clientRepository.save(client);
+		sendCreateClientMessage(client);
 	}
 
 	@Override
-	public void modifyClientName(String email, String name) {
-		// TODO Auto-generated method stub
-		
+	public void deleteClient(String mail) {
+		clientRepository.delete(clientRepository.findByMail(mail).get(0));
 	}
 
 	@Override
-	public void modifyClientAdress(String email, String Adress) {
-		// TODO Auto-generated method stub
-		
+	public void modifyClientName(String mail, String name) {
+		Client client = clientRepository.findByMail(mail).get(0);
+		client.setName(name);
+		clientRepository.delete(client);
+		clientRepository.insert(client);
 	}
 
 	@Override
-	public void modifyClientPhone(String email, int phoneNumber) {
-		// TODO Auto-generated method stub
-		
+	public void modifyClientAddress(String mail, String address) {
+		Client client = clientRepository.findByMail(mail).get(0);
+		client.setAddress(address);
+		clientRepository.delete(client);
+		clientRepository.insert(client);
+	}
+
+	@Override
+	public void modifyClientPhone(String mail, int phoneNumber) {
+		Client client = clientRepository.findByMail(mail).get(0);
+		client.setNumber(phoneNumber);
+		clientRepository.delete(client);
+		clientRepository.insert(client);
 	}
 
 }
--- clients/clients-accounts-modification/src/main/java/com/lama/mse/clients/modification/accounts/service/IClientService.java
+++ clients/clients-accounts-modification/src/main/java/com/lama/mse/clients/modification/accounts/service/IClientService.java
@@ -1,11 +1,13 @@
 package com.lama.mse.clients.modification.accounts.service;
 
+import com.lama.mse.clients.modification.accounts.model.Client;
+
 public interface IClientService {
 	
-	public void addClient(String email);
-	public void deleteClient(String email);
-	public void modifyClientName(String email, String name);
-	public void modifyClientAdress(String email, String Adress);
-	public void modifyClientPhone(String email, int phoneNumber);
+	public void addClient(Client client);
+	public void deleteClient(String mail);
+	public void modifyClientName(String mail, String name);
+	public void modifyClientAddress(String mail, String Adress);
+	public void modifyClientPhone(String mail, int phoneNumber);
 	
 }
