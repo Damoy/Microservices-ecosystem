e93db9f6195e7fa36af8bb49b020087da90fe8a3
FORNALI Damien <itsdamoy@gmail.com>
2018-10-22 17:56:15 +0200

    Start of clients orders addition

--- clients/clients-orders-addition/src/main/java/com/lama/mse/clients/orders/addition/Application.java
+++
@@ -1,9 +0,0 @@
-package com.lama.mse.clients.orders.addition;
-
-public class Application {
-
-	public static void main(String[] args){
-		
-	}
-	
-}
---
+++ clients/clients-orders-addition/src/main/java/com/lama/mse/clients/orders/addition/app/Application.java
@@ -0,0 +1,17 @@
+package com.lama.mse.clients.orders.addition.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+		"", 
+		"",
+		""
+})
+public class Application {
+
+	public static void main(String[] args){
+		SpringApplication.run(Application.class);
+	}
+	
+}
