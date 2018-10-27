dd0cdb6468ad5981ff085cc25e9c8893d4f14526
FORNALI Damien <itsdamoy@gmail.com>
2018-10-27 18:49:58 +0200

    restaurants consultation orders

--- restaurants/restaurants-consultation/restaurants-consultation-orders/src/main/java/com/lama/mse/restaurants/consultation/orders/app/Application.java
+++ restaurants/restaurants-consultation/restaurants-consultation-orders/src/main/java/com/lama/mse/restaurants/consultation/orders/app/Application.java
@@ -1,9 +1,22 @@
 package com.lama.mse.restaurants.consultation.orders.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@SpringBootApplication(scanBasePackages = {
+		"com.lama.mse.restaurants.consultation.orders.app",
+		"com.lama.mse.restaurants.consultation.orders.controller",
+		"com.lama.mse.restaurants.consultation.orders.kafka",
+		"com.lama.mse.restaurants.consultation.orders.model",
+		"com.lama.mse.restaurants.consultation.orders.repository",
+		"com.lama.mse.restaurants.consultation.orders.service"
+})
+@EnableMongoRepositories("com.lama.mse.restaurants.consultation.orders.repository")
 public class Application {
 
 	public static void main(String[] args) {
-
+		SpringApplication.run(Application.class);
 	}
+
 }
