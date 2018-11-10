71743619a7c6d625066acfe69eb1905c780e9ae3
Joanna Swiderska <joanna.swiderskasia@gmail.com>
2018-11-10 18:56:52 +0100

    coursier's controller changments (kafka)

--- coursiers/coursiers-controller/src/main/java/com/lama/mse/clients/controller/kafka/KafkaIO.java
+++ coursiers/coursiers-controller/src/main/java/com/lama/mse/clients/controller/kafka/KafkaIO.java
@@ -2,7 +2,9 @@ package com.lama.mse.clients.controller.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @Component
 public class KafkaIO {
@@ -14,11 +16,12 @@ public class KafkaIO {
 		
 	}
 	
-	public void sendCreateCoursierRequest(String orderJson) {
-		kafka.send("create-coursier", orderJson);
-	}
+	public ListenableFuture<SendResult<String, String>> sendCreateCoursierRequest(String orderJson) {
+		ListenableFuture<SendResult<String, String>> future = kafka.send("create-coursier", orderJson);
+		//Logs.infoln("Sent create-coursier.");
+		return future;	}
 	
-	public void sendEditCoursierRequest(String mail, String coursierAttribute, String attributeValue) {
+	public ListenableFuture<SendResult<String, String>> sendEditCoursierRequest(String mail, String coursierAttribute, String attributeValue) {
 		String verb = "edit-coursier";
 		
 		if(coursierAttribute != null) {
@@ -26,21 +29,30 @@ public class KafkaIO {
 		}
 		
 		String requestContent = mail + ";" + attributeValue;
-		kafka.send(verb, requestContent);
+		ListenableFuture<SendResult<String, String>> future = kafka.send(verb, requestContent);
+		//Logs.infoln("Sent edit-coursier");
+		return future;
 	}
 
-	public void sendEditOrderStatusCoursierRequest(long orderId){
+	public ListenableFuture<SendResult<String, String>> sendEditOrderStatusCoursierRequest(long orderId){
 		//String requestContent coursierId + orderId
-		kafka.send("delivered-order-coursier", "" + orderId);
+		ListenableFuture<SendResult<String, String>> future = kafka.send("delivered-order-coursier", "" + orderId);
+		//Logs.infoln("Sent delivered-order-coursier");
+		return future;
 	}
 
-	public void sendAccidentCoursierRequest(String cause){
-		kafka.send("accident-coursier", cause);
+	public ListenableFuture<SendResult<String, String>> sendAccidentCoursierRequest(String cause){
+		ListenableFuture<SendResult<String, String>> future = kafka.send("accident-coursier", cause);
+		//Logs.infoln("Sent accident-coursier");
+		return future;
 	}
 
-	public void sendOrdersAroundMetCoursierRequest(String cause){
-		kafka.send("consult-coursier-orders-aroundme", cause);
+	public ListenableFuture<SendResult<String, String>> sendOrdersAroundMetCoursierRequest(String cause){
+		ListenableFuture<SendResult<String, String>> future = kafka.send("consult-coursier-orders-aroundme", cause);
+		//Logs.infoln("Sent consult-coursier-orders-aroundme");
+		return future;
 	}
+
 
 
 }
