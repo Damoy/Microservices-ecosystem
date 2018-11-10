7b9a65a32efaffd3a8a84bc2ba63a7de938a498d
Amandine Benza <amandine.benza@etu.unice.fr>
2018-11-10 22:30:54 +0100

    Coursiers

--- coursiers/coursiers-orders/src/main/java/com/lama/mse/coursiers/orders/repository/IOrderRepository.java
+++ coursiers/coursiers-orders/src/main/java/com/lama/mse/coursiers/orders/repository/IOrderRepository.java
@@ -1,9 +1,14 @@
 package com.lama.mse.coursiers.orders.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.lama.mse.coursiers.orders.model.Order;
 
-public interface IOrderRepository extends MongoRepository<Order, Integer> {
-
+public interface IOrderRepository extends MongoRepository<Order, String> {
+	
+	public List<Order> findByCoursierMail(String coursierMail);
+	public Order findById(int id);
+	
 }
--- coursiers/coursiers-orders/src/main/java/com/lama/mse/coursiers/orders/service/OrderService.java
+++ coursiers/coursiers-orders/src/main/java/com/lama/mse/coursiers/orders/service/OrderService.java
@@ -11,7 +11,6 @@ import com.lama.mse.coursiers.orders.kafka.KafkaIO;
 import com.lama.mse.coursiers.orders.model.Order;
 import com.lama.mse.coursiers.orders.repository.IOrderRepository;
 
-
 @Service("OrderService")
 public class OrderService implements IOrderService {
 
@@ -20,21 +19,20 @@ public class OrderService implements IOrderService {
 
 	@Autowired
 	private KafkaIO kafkaIO;
-	
-	public OrderService(){
+
+	public OrderService() {
 	}
 
 	@Override
-	public Order findById(long id) {
-		Order order = orderRepository.findById((int)id).get();
-		return order;
-
+	public List<Order> findByCoursierMail(String coursierMail) {
+		return orderRepository.findByCoursierMail(coursierMail);
 	}
 
 	@Override
 	public List<Order> getOrdersAroundMe() {
-		List<Order> orders = new ArrayList<>(  );
-		orders =  orderRepository.findAll().stream().filter( order -> order.getAroundMe() == true).collect( Collectors.toList());
+		List<Order> orders = new ArrayList<>();
+		orders = orderRepository.findAll().stream().filter(order -> order.isAroundMe() == true)
+				.collect(Collectors.toList());
 		return orders;
 	}
 
@@ -43,48 +41,43 @@ public class OrderService implements IOrderService {
 		return orderRepository.findAll();
 	}
 
-
 	@Override
-	public void saveOrder(Order order){
+	public void saveOrder(Order order) {
 		orderRepository.save(order);
 	}
 
 	@Override
 	public void deleteOrder(long id) {
-		Order order = orderRepository.findById( (int)id ).get();
-		orderRepository.delete( order );
-		kafkaIO.sendDeletedOrderMessage( order );
+		Order order = orderRepository.findById((int) id);
+		orderRepository.delete(order);
+		kafkaIO.sendDeletedOrderMessage(order);
 	}
 
 	@Override
-	public void modifyClientMail(long id, String mail){
-		Order order = orderRepository.findById( (int)id ).get();
+	public void modifyClientMail(long id, String mail) {
+		Order order = orderRepository.findById((int) id);
 		order.setClientMail(mail);
-		kafkaIO.sendDeletedOrderMessage( order );
+		kafkaIO.sendDeletedOrderMessage(order);
 	}
 
 	@Override
-	public void modifyRestaurantName(long id, String restaurantName){
-		Order order = orderRepository.findById( (int)id ).get();
+	public void modifyRestaurantName(long id, String restaurantName) {
+		Order order = orderRepository.findById((int) id);
 		order.setRestaurantName(restaurantName);
-		kafkaIO.sendDeletedOrderMessage( order );
+		kafkaIO.sendDeletedOrderMessage(order);
 	}
 
 	@Override
-	public void modifyDeliveryLocation(long id, String deliveryLocation){
-		Order order = orderRepository.findById( (int)id ).get();
+	public void modifyDeliveryLocation(long id, String deliveryLocation) {
+		Order order = orderRepository.findById((int) id);
 		order.setDeliveryLocation(deliveryLocation);
-		kafkaIO.sendDeletedOrderMessage( order );
+		kafkaIO.sendDeletedOrderMessage(order);
 	}
 
 	@Override
-	public void modifyFoodNames(long id, List<String> foodNames){
-		Order order = orderRepository.findById( (int)id ).get();
+	public void modifyFoodNames(long id, List<String> foodNames) {
+		Order order = orderRepository.findById((int) id);
 		order.setFoodNames(foodNames);
-		kafkaIO.sendDeletedOrderMessage( order );
+		kafkaIO.sendDeletedOrderMessage(order);
 	}
-
-
-
-
 }
