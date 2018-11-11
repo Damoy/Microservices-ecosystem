ad63f849797b7d1730c007b64da6c771ec91db9c
Amandine Benza <amandine.benza@etu.unice.fr>
2018-11-11 12:13:22 +0100

    Run

--- run.sh
+++ run.sh
@@ -31,11 +31,32 @@ sleep $personasSleep;
 echo "";
 firstStorySleep=2;
 
-echo ">> Starting first story"; 
-echo "1. As $student or $dev I can order my lunch from a restaurant so that the food is delivered to my place."
+printf ">> Starting first story";
 sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n"; echo "";
-
+echo "1. As $student or $dev I can order my lunch from a restaurant so that the food is delivered to my place.";
+sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n"; echo "";
 echo "CURL TODO"; sleep $firstStorySleep;
 
-echo "2. As $student, I can browse the food catalogue by categories so that I can immediately identify my favorite junk food."
-sleep $firstStorySleep;
+printf "2. As $student, I can browse the food catalogue by categories so that I can immediately identify my favorite junk food.";
+sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n"; echo "";
+echo "CURL TODO"; sleep $firstStorySleep;
+
+printf "3. As $dev, I want to know before ordering the estimated time of delivery of the meal so that I can schedule my work around it, and be ready when it arrives.";
+sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n"; echo "";
+echo "CURL TODO"; sleep $firstStorySleep;
+
+printf "4. As Erin, I can pay directly by credit card on the platform, so that I only have to retrieve my food when delivered.";
+sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n"; echo "";
+echo "CURL TODO"; sleep $firstStorySleep;
+
+printf "5. As Jordan, I want to access to the order list, so that I can prepare the meal efficiently.";
+sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n"; echo "";
+echo "CURL TODO"; sleep $firstStorySleep;
+
+printf "6. As Jamie, I want to know the orders that will have to be delivered around me, so that I can choose one and go to the restaurant to begin the course.";
+sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n"; echo "";
+echo "CURL TODO"; sleep $firstStorySleep;
+
+printf "7. As Jamie, I want to notify that the order has been delivered, so that my account can be credited and the restaurant can be informed.";
+sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n"; echo "";
+echo "CURL TODO"; sleep $firstStorySleep;
